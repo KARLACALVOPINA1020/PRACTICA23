92e09205ffd6982fd4d6194e0b538f84a0fa484c
calvopinakarla <calvopinakarla06@gmail.com>
2025-12-14 18:11:38 -0500

    EJEMPLO

---
+++ OneDrive/Documents/NetBeansProjects/PRACTICA23/src/main/java/ec/edu/espoch/practica23/PRACTICA23.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practica23;
+
+/**
+ *
+ * @author HP
+ */
+public class PRACTICA23 {
+
+    public String nombre;
+}
