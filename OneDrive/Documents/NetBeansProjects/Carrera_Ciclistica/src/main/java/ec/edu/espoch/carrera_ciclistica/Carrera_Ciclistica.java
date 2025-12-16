f6777f10a927870009a7adf613c43055de68cdc7
calvopinakarla <calvopinakarla06@gmail.com>
2025-12-16 11:51:04 -0500

    PRACTICA

---
+++ OneDrive/Documents/NetBeansProjects/Carrera_Ciclistica/src/main/java/ec/edu/espoch/carrera_ciclistica/Carrera_Ciclistica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.carrera_ciclistica;
+
+/**
+ *
+ * @author HP
+ */
+public class Carrera_Ciclistica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ OneDrive/Documents/NetBeansProjects/Carrera_Ciclistica/src/main/java/ec/edu/espoch/carrera_ciclistica/Ciclismo.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.carrera_ciclistica;
+
+/**
+ *
+ * @author HP
+ */
+public class Ciclismo {
+    private int identificador; 
+    private String nombre; 
+    private boolean tiempoAcumulado; 
+
+    public Ciclismo(int identificador, String nombre, boolean tiempoAcumulado) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.tiempoAcumulado = tiempoAcumulado;
+    }
+    
+    public int getidentificador(){
+    return identificador;
+    }
+    public int setidentificador (float identificador){
+    this.identificador
+    }
+    public String getNombre(){
+    return nombre;
+    }
+    public String setNombre (float nombre){
+    this.nombre;  
+    }
+    
+    public String imprimirTipo;
+
+
+    
+}
